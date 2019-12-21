a8bda5063d8b277d6e1bce0d6e39ee4d3026920a
11182 <11182@AQL0908-5490W10.AQ.Local>
2019-12-21 13:36:51 +0530

    Completes the @elementcollection example

---
+++ hibernate-advance-concept-01/src/test/java/in/bushansirgur/hibernate/ElementCollectionDemo.java
@@ -0,0 +1,49 @@
+package in.bushansirgur.hibernate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import in.bushansirgur.hibernate.model.EmployeeModel;
+
+public class ElementCollectionDemo {
+	
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(EmployeeModel.class).buildSessionFactory();
+		
+		//get the current session
+		Session session = factory.getCurrentSession();
+		
+		EmployeeModel employee = null;
+		try {
+			//create hashset and add phone numbers
+			Set<String> phoneNumbers = new HashSet<String>();
+			phoneNumbers.add("9988776655");
+			phoneNumbers.add("9876543210");
+			phoneNumbers.add("8899776655");
+			phoneNumbers.add("9876543210");
+			
+			//create employee object
+			employee = new EmployeeModel("Bushan", "bushan@bushansirgur.in", phoneNumbers);
+			
+			//start transaction
+			session.beginTransaction();
+			
+			//save object
+			session.persist(employee);
+			
+			//commit the transactions
+			session.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			//close session and session factory
+			session.close();
+			factory.close();
+		}
+	}
+}
---
+++ hibernate-advance-concept-01/src/test/java/in/bushansirgur/hibernate/model/EmployeeModel.java
@@ -0,0 +1,80 @@
+package in.bushansirgur.hibernate.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class EmployeeModel {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private int id;
+	
+	@Column(name="full_name")
+	private String name;
+	
+	@Column(name="email_id")
+	private String email;
+	
+	@ElementCollection
+	@CollectionTable(name="emplooyee_phone_numbers", joinColumns=@JoinColumn(name="employee_id"))
+	@Column(name="phone_number")
+	private Set<String> phoneNumbers;
+
+	public EmployeeModel(String name, String email, Set<String> phoneNumbers) {
+		this.name = name;
+		this.email = email;
+		this.phoneNumbers = phoneNumbers;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Set<String> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeModel [id=" + id + ", name=" + name + ", email=" + email + ", phoneNumbers=" + phoneNumbers
+				+ "]";
+	}
+	
+}
